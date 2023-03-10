259ace85a3052713645e96d8fd8668e84012809b
rryanlee <leeryann5@gmail.com>
2023-03-10 13:30:43 -0800

    fixed and added test cases for preexisting testing files

--- tests/test_clean_data.py
+++ tests/test_clean_data.py
@@ -1,10 +1,14 @@
+import pytest
 import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from pandas.api.types import CategoricalDtype
 
 sys.path.append('src')
 from clean_data import relabel_bclass, col_dtype_reformat
 
-def test_relabel_bclass():
-    # Create Sample DataFrame
+def test_relabel_bclass_returns():
     pre_df = pd.DataFrame({'age': [1,2,3,4,5,6,7,8,9,10],
                            'binaryClass': ['negative.|3733', 'negative.|1442', 'n',
                                            'nABC123', 'hyperthyroid.|108', 'h',
@@ -13,22 +17,39 @@ def test_relabel_bclass():
                            'binaryClass': ['P', 'P', 'P',
                                            'P', 'N', 'N',
                                            np.nan, np.nan, np.nan, np.nan]})
-    post_df = relabel_bclass(pre_df)
-    # Assert that only expected values were found in column
-    assert(post_df.binaryClass.unique().tolist() == ['P', 'N', np.nan])
-    # Assert that changes were as expected
-    assert(post_df.equals(ref_df))
+    assert isinstance(relabel_bclass(pre_df), pd.DataFrame)
 
-def test_col_dtype_reformat():
-    num_cols = ['num1', 'num2', 'num3', 'abc']
-    cat_cols = ['cat1', 'cat2']
+def test_relabel_bclass_relabels():
+    pre_df = pd.DataFrame({'age': [1,2,3,4,5,6,7,8,9,10],
+                           'binaryClass': ['negative.|3733', 'negative.|1442', 'n',
+                                           'nABC123', 'hyperthyroid.|108', 'h',
+                                           'T3 toxic.|1304', 'T', 'goitre.|2416', 'g']})
+    ref_df = pd.DataFrame({'age': [1,2,3,4,5,6,7,8,9,10],
+                           'binaryClass':  ['P', 'P', 'P',
+                                           'P', 'N', 'N',
+                                           np.nan, np.nan, np.nan, np.nan]})
+    result = relabel_bclass(pre_df)
+    pd.testing.assert_frame_equal(result, ref_df)
 
-    pre_df = pd.DataFrame(columns = ['num1', 'num2', 'abc', 'cat1', 'cat2', 'num3'],
-                          dtype=object)
+def test_data_col_dtype_reformat():
+    return pd.DataFrame({'age': ['41','23','46','70'],
+                        'sex': ['F','F','M','F'],
+                        'TSH':['1.3','4.1','0.98','0.16'],
+                        'hypopituitary': ['f','f','f','f']})
 
-    post_df = col_dtype_reformat(num_cols, cat_cols, pre_df)
+def test_col_dtype_reformat_returns():
+    num = ['age','TSH']
+    cat = []
+    result = col_dtype_reformat(num, cat, test_data_col_dtype_reformat())
+    assert isinstance(result, pd.DataFrame)
 
-    # Assert the columns are casted to the correct dtypes
-    assert((post_df[num_cols].dtypes == np.float64).all())
-    assert((post_df[cat_cols].dtypes == 'category').all())
-    assert((post_df['num3'].dtypes == np.int64).all())
+def test_col_dtype_reformat_datatypes():
+    num = ['age','TSH']
+    cat = []
+    result = col_dtype_reformat(num, cat, test_data_col_dtype_reformat())
+    actual_types = result.dtypes
+    expected_types = pd.Series({'age': 'float64',
+                                'sex': 'object',
+                                'TSH': 'float64',
+                                'hypopituitary': 'object'})
+    assert actual_types.equals(expected_types)
